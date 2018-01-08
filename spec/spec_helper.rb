9e3a0035fbfe8f9f75356241af1303398ecdecbf
Eustaquio122 <tiagofsousa@gmail.com>
2018-01-08 14:29:30 +0000

    .rspec
    Sets up RSpec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,17 @@
+require 'rspec'
+require 'simplecov'
+require 'simplecov-console'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+  SimpleCov::Formatter::HTMLFormatter
+])
+SimpleCov.start
+
+RSpec.configure do |config|
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+end
