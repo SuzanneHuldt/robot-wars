5ae9f63c2b216e33e9cdabdb4e18c9a80511a7f0
Peter Johnstone <peter.wdj@gmail.com>
2018-01-10 15:22:14 +0000

    Fix rubocop errors

--- spec/bot_spec.rb
+++ spec/bot_spec.rb
@@ -1,6 +1,6 @@
 describe Bot do
   before do
-    $stdin = StringIO.new("Test String")
+    $stdin = StringIO.new('Test String')
   end
 
   after do
@@ -9,14 +9,14 @@ describe Bot do
 
   describe '#receive' do
     it 'should receive and output a string' do
-      expect(subject.receive).to eq("Test String")
+      expect(subject.receive).to eq 'Test String'
     end
   end
 
   describe '#return_string' do
     it 'should return a string to the engine' do
       subject.receive
-      expect{ subject.return_string }.to output("Test String").to_stdout
+      expect { subject.return_string }.to output 'Test String'.to_stdout
     end
   end
 end
