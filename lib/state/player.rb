16c2bd487898b0c16f5556d478faf8dc9aa9694b
Peter Johnstone <peter.wdj@gmail.com>
2018-01-10 16:46:35 +0000

    Pass test for last_move property

--- lib/state/player.rb
+++ lib/state/player.rb
@@ -1,10 +1,11 @@
 class Player
 
-  attr_reader :name, :living_cells
+  attr_reader :name, :living_cells, :last_move
 
-  def initialize(name, living_cells)
+  def initialize(name, living_cells, last_move=nil)
     @name = name
     @living_cells = living_cells
+    @last_move = last_move
   end
 
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,7 +1,7 @@
 require 'state/player'
 
 describe Player do
-  let(:player) { Player.new('Marvin', 50) }
+  let(:player) { Player.new('Marvin', 50, 'pass') }
 
   describe '#initialize' do
     it 'should have a name property' do
@@ -10,6 +10,10 @@ describe Player do
 
     it 'should have a living_cells property' do
       expect(player.living_cells).to eq 50
+    end
+
+    it 'should have a last_move property' do
+      expect(player.last_move).to eq 'pass'
     end
   end
 end
