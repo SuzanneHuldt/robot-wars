f1e512c2918d69e9233f1098cb54dff1a9463f02
Peter Johnstone <peter.wdj@gmail.com>
2018-01-11 14:59:56 +0000

    Created test stubbing randomness in generate_move

--- spec/action_spec.rb
+++ spec/action_spec.rb
@@ -1,2 +1,8 @@
 describe Action do
+  describe '#generate_move' do
+    it 'should be an instance of String' do
+      stub_const("Action::MOVES", ['pass'])
+      expect(subject.generate_move({})).to eq 'pass'
+    end
+  end
 end
