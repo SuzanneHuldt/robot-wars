2bf21c13ad739c8b2b90eeeb953160378453b0dd
Eustaquio122 <tiagofsousa@gmail.com>
2018-01-11 17:28:31 +0000

    Refactors formatter.rb

--- lib/formatter.rb
+++ lib/formatter.rb
@@ -1,15 +1,12 @@
 class Formatter
-  def format_input(input_line)
-    input_line.gsub!(/,/, ' ') if input_line.start_with?('player_names')
-    input_line.split(' ')
-  end
+  class << self
+    def format_input(input_line)
+      input_line.gsub!(/,/, ' ') if input_line.start_with?('player_names')
+      input_line.split(' ')
+    end
 
-  def action_format(input_array)
-    input_array[2].to_i
-  end
-
-  def format_output(output_line)
-    # format from Action accordingly
-    output_line
+    def format_output(action)
+      action.join(' ') + "\n"
+    end
   end
 end
