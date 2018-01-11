9a6552104ad7f4e9f47a2b025c4ee21483b1b249
Robert Clayton <robbie.c.clayton@gmail.com>
2018-01-11 18:58:10 +0000

    randomises coordinates with random_move

--- lib/generate_move.rb
+++ lib/generate_move.rb
@@ -7,7 +7,24 @@ class GenerateMove
     @game_state = hash
   end
 
-  def generate
-    move_type = MOVES.sample
+  def random_move
+    move_type = generate_action
+    return 'pass' if move_type == 'pass'
+    return assign_kill_coordinates if move_type == 'kill'
+    return assign_birth_coordinates if move_type == 'birth'
+  end
+
+  def generate_action
+    MOVES.sample
+  end
+
+  def assign_kill_coordinates
+    "#{rand(@game_state[:field_height])},#{rand(@game_state[:field_width])}"
+  end
+
+  def assign_birth_coordinates
+    [ "#{rand(@game_state[:field_height])},#{rand(@game_state[:field_width])}",
+      "#{rand(@game_state[:field_height])},#{rand(@game_state[:field_width])}",
+      "#{rand(@game_state[:field_height])},#{rand(@game_state[:field_width])}" ]
   end
 end
