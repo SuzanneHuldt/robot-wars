2b43302fff6522ad82c8e4a81888afe0430c3e9b
Peter Johnstone <peter.wdj@gmail.com>
2018-01-11 15:00:16 +0000

    Test code within #generate_move

--- lib/action.rb
+++ lib/action.rb
@@ -1,17 +1,20 @@
 class Action
+  MOVES = ['birth', 'kill', 'pass']
+
   def new_action(t, hash)
     # t = time
-#    true
+    # true
     generate_move(hash)
 
   end
 
   def legal_move(move, hash)
 
-    #returns boolean
+    # returns boolean
   end
 
   def generate_move(hash)
+    MOVES.sample
     # code to generate a move
     # move = (GENERATED_MOVE)
     # return generate_move(hash) unless legal_move(move, hash)
@@ -24,13 +27,13 @@ class Action
 
 
   def on_board(move, hash)
-    #for now this functionality exists within the generate move method (for random move only)
-    #it will be scooped into the legal move class eventually
-    #always the same
+    # for now this functionality exists within the generate move method (for random move only)
+    # it will be scooped into the legal move class eventually
+    # always the same
   end
 
   def legal_cell(move, hash)
-    #going to differ depending on move type
+    # going to differ depending on move type
   end
 
 
