ce6589c861b804abc679bacfb6ab908546a7b6dd
Peter Johnstone <peter.wdj@gmail.com>
2018-01-11 15:05:06 +0000

    Add passing stubbed tests for selecting move type

--- spec/action_spec.rb
+++ spec/action_spec.rb
@@ -1,8 +1,16 @@
 describe Action do
   describe '#generate_move' do
-    it 'should be an instance of String' do
-      stub_const("Action::MOVES", ['pass'])
+    it 'should return pass' do
+      stub_const('Action::MOVES', ['pass'])
       expect(subject.generate_move({})).to eq 'pass'
+    end
+    it 'should return birth' do
+      stub_const('Action::MOVES', ['birth'])
+      expect(subject.generate_move({})).to eq 'birth'
+    end
+    it 'should return kill' do
+      stub_const('Action::MOVES', ['kill'])
+      expect(subject.generate_move({})).to eq 'kill'
     end
   end
 end
