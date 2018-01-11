fd9a179b66f419c2221bbd091b98ed6b3fd0fb89
Suzanne Huldt <suzanne.huldt.16@ucl.ac.uk>
2018-01-11 15:52:58 +0000

    skeleton methods

---
+++ lib/legalmove.rb
@@ -0,0 +1,21 @@
+class LegalMove
+
+  def intialize(move, hash)
+    @move = move
+    @hash = hash
+  end
+
+  def legal_kill(move, hash)
+
+  end
+
+  def legal_birth(move, hash)
+  end
+
+  def filled_cell(move, hash)
+  end
+
+  def owned_cell(move, hash)
+    
+  end
+end
