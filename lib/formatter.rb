a61c2368f2c59a7f22f1a34c73716e4d4c00188a
Eustaquio122 <tiagofsousa@gmail.com>
2018-01-12 11:48:20 +0000

    Changed delete call in formatter to delete

--- lib/formatter.rb
+++ lib/formatter.rb
@@ -1,7 +1,7 @@
 class Formatter
   def format_input(input_line)
-    input_line.gsub!(/,/, ' ') if input_line.include?('player_names')
-    input_line.gsub!(/,/, '') if input_line.include?('game board')
+    input_line.tr!(',', ' ') if input_line.include?('player_names')
+    input_line.delete!(',') if input_line.include?('game board')
     input_line.split(' ')
   end
 
