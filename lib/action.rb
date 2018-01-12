8888c4ce26e17aee2fb404e15869750235daff14
Peter Johnstone <peter.wdj@gmail.com>
2018-01-12 11:50:23 +0000

    Cleared out redundant code

--- lib/action.rb
+++ lib/action.rb
@@ -1,49 +1,17 @@
 class Action
-  MOVES = ['birth', 'kill', 'pass']
-
   def new_action(t, hash)
-    # t = time
-    # true
-    generate_move(hash)
 
   end
 
   def legal_move(move, hash)
 
-    # returns boolean
   end
 
   def generate_move(hash)
-    # generate_move.new()
-    move = MOVES.sample
-    return move if move == 'pass'
-    return assign_kill_coordinates(hash) if move == 'kill'
-    # code to generate a move
-    # move = (GENERATED_MOVE)
-    # return generate_move(hash) unless legal_move(move, hash)
-    move
+
   end
 
   def export_move(move)
 
   end
-
-
-  # def on_board(move, hash)
-  #   # for now this functionality exists within the generate move method (for random move only)
-  #   # it will be scooped into the legal move class eventually
-  #   # always the same
-  # end
-  #
-  # def legal_cell(move, hash)
-  #   # going to differ depending on move type
-  # end
-  #
-  # private
-  #
-  # def assign_kill_coordinates(hash)
-  #
-  # end
-
-
 end
