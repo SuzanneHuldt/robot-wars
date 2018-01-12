c72ff7748cc9811e98c34ca069366cb0a3346f4b
Suzanne Huldt <suzanne.huldt.16@ucl.ac.uk>
2018-01-12 18:48:32 +0000

    clear horizontal and clear vertical tests apassing yx system

---
+++ lib/birthpattern.rb
@@ -0,0 +1,40 @@
+class BirthPattern
+
+  attr_reader :a
+  def initialize
+
+  end
+
+  def match(field)
+
+  end
+
+  #get rid 
+  def set_start(field, x, y)
+    @a = field[y][x]
+  end
+
+  def clear_horizontal(field, x, y)
+    hits = []
+    z = 1
+    while z < 5 do
+      x += 1
+      z += 1
+      hits << "hit" if field[y][x] != "."
+    end
+    hits.length == 0
+  end
+
+  def clear_vertical(field, x, y)
+    hits = []
+    z = 1
+    while z < 5 do
+      y += 1
+      z += 1
+      hits << "hit" if field[y][x] != "."
+    end
+    hits.length == 0
+  end
+
+
+end
---
+++ spec/birthpattern_spec.rb
@@ -0,0 +1,44 @@
+describe BirthPattern do
+
+#REMEMBER Y REFACTOR
+
+  before (:each) do
+    @field_1 = [
+      ['0','.','.','.','.'],['.','1','1','.','.'],['.','.','.','.','.'],['.','.','.','.','.'],
+      ['.','.','.','.','.'],['1','.','.','.','.'],['.','1','1','.','.'],['.','.','.','.','.'],['.','.','.','.','.'],
+      ['.','.','.','.','.']
+    ]
+
+    @field_2 = [
+      ['0','.','1','.','.'],['.','1','1','.','.'],['1','.','.','.','.'],['.','.','.','.','.'],
+      ['.','.','.','.','.'],['1','.','.','.','.'],['.','1','1','.','.'],['.','.','.','.','.'],['.','.','.','.','.'],
+      ['.','.','.','.','.']
+    ]
+  end
+
+  describe "#set start" do
+    it "sets a to the given element of the given subarray" do
+      subject.set_start(@field_1,0,0)
+      expect(subject.a).to eq('0')
+    end
+  end
+
+  describe "#clear horizontal" do
+    it "returns true if a's next three horizontal cells are empty" do
+      expect(subject.clear_horizontal(@field_1,0,0)).to be(true)
+    end
+    it "returns false if a's next three horizontal cells are not" do
+      expect(subject.clear_horizontal(@field_2,0,0)).to be(false)
+    end
+  end
+
+  describe "#clear vertical" do
+    it "returns true if a's next three vertical cells are empty" do
+      expect(subject.clear_vertical(@field_1,0,0)).to be(true)
+    end
+    it "returns false if a's next three vertical cells are empty" do
+      expect(subject.clear_vertical(@field_2,0,0)).to be(false)
+    end
+  end
+
+end
