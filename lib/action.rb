a29586b2e065cd5e250a592e93b901bd9ca57392
Peter Johnstone <peter.wdj@gmail.com>
2018-01-12 11:51:11 +0000

    Revert "Suze thurs"

--- lib/action.rb
+++ lib/action.rb
@@ -1,17 +1,6 @@
 class Action
   def new_action(t, hash)
-
-  end
-
-  def legal_move(move, hash)
-
-  end
-
-  def generate_move(hash)
-
-  end
-
-  def export_move(move)
-
+    # t = time
+    true
   end
 end
