fa7e6fcb6a2d8b9ab2402d0dac26e7abf424e72a
Eustaquio122 <tiagofsousa@gmail.com>
2018-01-13 20:34:23 +0000

    Adds 'move' folder to 'lib' with new random_move file

---
+++ lib/move/random_move.rb
@@ -0,0 +1,38 @@
+class RandomMove
+    MOVES = %w(kill birth pass)
+
+    def new_move(my_id, op_id, board)
+      @board = board
+      @my_cells, @op_cells, @empty_cells = get_cells(my_id, op_id)
+      @my_cells.length >= 2 ? move = MOVES.sample : ['kill', 'pass'].sample
+      self.send(move)
+    end
+
+    def kill
+      ['kill', @op_cells.sample]
+    end
+
+    def birth
+      @my_cells.shuffle!
+      ['birth', @empty_cells.sample, @my_cells.shift, @my_cells.shift]
+
+    end
+
+    def pass
+      ['pass']
+    end
+
+    def get_cells(my_id, op_id)
+      [filter_cells(my_id), filter_cells(op_id), filter_cells('.')]
+    end
+
+    def filter_cells(id)
+      cells = []
+      @board.each_with_index do |row, y|
+        row.each_with_index do |cell, x|
+          cells << "#{x},#{y}" if cell == id
+        end
+      end
+      cells
+    end
+end
