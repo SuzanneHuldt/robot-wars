0750d1ad3f736ef142b84aa2478e0e1a7c847f97
Peter Johnstone <peter.wdj@gmail.com>
2018-01-13 20:26:43 +0000

    Refactor logic to private methods to find topleftmost cell coordinates

--- lib/matcher.rb
+++ lib/matcher.rb
@@ -1,15 +1,30 @@
 class Matcher
 
   def find(board)
+    find_y_coordinate(board)
+    find_x_coordinate(board[@y])
+
+    p board[@y][@x]
+    p board[@y][@x + 1]
+    p board[@y + 1][@x]
+    p board[@y + 1 ][@x + 1]
+  end
+
+  private
+
+  def find_y_coordinate(board)
     board.each do |row|
       if row.include? '1'
         @y = board.index(row).to_i
         break
       end
     end
-    board[@y].each do |column|
+  end
+
+  def find_x_coordinate(row)
+    row.each do |column|
       if column.include? '1'
-        @x = board[@y].index(column).to_i
+        @x = row.index(column).to_i
       end
     end
   end
