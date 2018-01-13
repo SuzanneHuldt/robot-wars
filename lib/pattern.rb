ce15c1d5a87389489f265161c507484d3a632eca
Peter Johnstone <peter.wdj@gmail.com>
2018-01-13 22:55:02 +0000

    Remove superfluous return value

--- lib/pattern.rb
+++ lib/pattern.rb
@@ -11,6 +11,5 @@ class Pattern
     variations << [[@x, @y], [@x + 1, @y], [@x + 1, @y + 1]]
     variations << [[@x, @y], [@x - 1, @y + 1], [@x, @y + 1]]
     variations << [[@x, @y], [@x, @y + 1], [@x + 1, @y + 1]]
-    variations
   end
 end
