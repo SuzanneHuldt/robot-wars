28f4042b463cf12b0b169e744839a69880cde8ca
Eustaquio122 <tiagofsousa@gmail.com>
2018-01-13 20:34:50 +0000

    Refactors action to call on random_move

--- lib/action.rb
+++ lib/action.rb
@@ -1,17 +1,12 @@
 class Action
-  def new_action(t, hash)
-    ['pass']
+  def initialize
+    @random = RandomMove.new
   end
 
-  def legal_move(move, hash)
-
-  end
-
-  def generate_move(hash)
-
-  end
-
-  def export_move(move)
-
+  def new_action(timebank, info)
+    @info = info
+    @my_id = @info[:your_botid].to_s
+    @op_id = @my_id == '0' ? '1' : '0'
+    @random.new_move(@my_id, @op_id, @info[:field])
   end
 end
