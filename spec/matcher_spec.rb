dda83f30810f07b65f5e651f1a42a9f8494ee626
Peter Johnstone <peter.wdj@gmail.com>
2018-01-13 21:29:41 +0000

    Add test to ensure false is returned if no pattern is found

--- spec/matcher_spec.rb
+++ spec/matcher_spec.rb
@@ -9,5 +9,14 @@ describe Matcher do
       ]
       expect(subject.find(board)).to eq true
     end
+    it 'no pattern found should return false' do
+      board = [
+        ['.','.','.','.'],
+        ['.','1','1','.'],
+        ['.','.','.','.'],
+        ['.','.','.','.']
+      ]
+      expect(subject.find(board)).to eq false
+    end
   end
 end
