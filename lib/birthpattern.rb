7d14d0576ffba8b25edd16140976e675fb38c15b
Suzanne Huldt <suzanne.huldt.16@ucl.ac.uk>
2018-01-14 22:26:03 +0000

    need to sort error on empty grid

--- lib/birthpattern.rb
+++ lib/birthpattern.rb
@@ -1,34 +1,28 @@
 class BirthPattern
 
-  def find_birth(field)
-    a = 0
-    while a < 36 do
-      x = 0
-      y = 0
-      z = 1
-      while z < 51 do
+  def find_birth(field,x,y)
+    for i in field do
+      for z in i do
         if match_square(field,x,y)
           return match_square(field,x,y)
         end
-        z += 1
         x += 1
       end
-      a += 1
     end
   end
 
-  
+  def birth_cell
+
+  end
+
 
   def match_square(field,x,y)
     arr = []
     arr << (clear_horizontal(field,x,y) && clear_horizontal(field, x,(y+3)))
     arr << (clear_vertical(field,x,y) && clear_vertical(field,(x+3),y))
     arr << (two_consecutive(field,x,(y+1)) ^ two_consecutive(field,x,(y+2)))
-    arr.include? false ? false : true
-  end
 
-  def birth_cell
-
+    p arr.include? false ? false : true
   end
 
 
@@ -61,7 +55,6 @@ class BirthPattern
       x += 1
       z += 1
       hits << x if field[y][x] == "0"
-      p hits if hits.length == 2
     end
     hits.length == 2 && hits[1] - hits[0] == 1
   end
