1454b98598a0cceb80a057364d34e9c4846de13e
Peter Johnstone <peter.wdj@gmail.com>
2018-01-14 18:47:32 +0000

    Extract hard-coded pattern to new PatternTemplate class

--- lib/pattern.rb
+++ lib/pattern.rb
@@ -3,12 +3,7 @@ class Pattern
   def initialize(y, x)
     @y = y
     @x = x
-    @pattern = [
-      [[@x, @y], [@x + 1, @y], [@x, @y + 1]],
-      [[@x, @y], [@x + 1, @y], [@x + 1, @y + 1]],
-      [[@x, @y], [@x - 1, @y + 1], [@x, @y + 1]],
-      [[@x, @y], [@x, @y + 1], [@x + 1, @y + 1]]
-    ]
+    @pattern = PatternTemplate.new.next_gen_square(@x, @y)
 
   end
 
