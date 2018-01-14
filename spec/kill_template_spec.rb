355750e92c6275061da79da5a3879d9bf7a59437
Peter Johnstone <peter.wdj@gmail.com>
2018-01-14 22:16:21 +0000

    Add passing test for boat pattern

--- spec/kill_template_spec.rb
+++ spec/kill_template_spec.rb
@@ -43,4 +43,15 @@ describe KillTemplate do
       ]
     end
   end
+
+  describe '#boat' do
+    it 'returns an array of all variations of boat pattern' do
+      expect(subject.boat(1,1)).to eq [
+        [[1, 1], [2, 1], [1, 2], [3, 2], [2, 3]],
+        [[1, 1], [2, 1], [0, 2], [2, 2], [1, 3]],
+        [[1, 1], [0, 2], [2, 2], [1, 3], [2, 3]],
+        [[1, 1], [0, 2], [2, 2], [0, 3], [1, 3]]
+      ]
+    end
+  end
 end
