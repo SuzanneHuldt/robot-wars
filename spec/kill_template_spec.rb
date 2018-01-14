16be82c9324b3373060961e8d181f9514d46c669
Peter Johnstone <peter.wdj@gmail.com>
2018-01-14 21:35:08 +0000

    Add tests for stable templates listed on Riddles.io website for GOLAD

--- spec/kill_template_spec.rb
+++ spec/kill_template_spec.rb
@@ -1,11 +1,37 @@
 describe KillTemplate do
   describe '#next_gen_square' do
-    it 'should return an array of all variations that will lead to a square on the next generation' do
+    it 'returns an array of all variations of a next_gen_square' do
       expect(subject.next_gen_square(1,1)).to eq [
         [[1, 1], [2, 1], [1, 2]],
         [[1, 1], [2, 1], [2, 2]],
         [[1, 1], [0, 2], [1, 2]],
         [[1, 1], [1, 2], [2, 2]]
+      ]
+    end
+  end
+
+  describe '#block' do
+    it 'returns an array of all variations of tub pattern' do
+      expect(subject.tub(1,1)).to eq [
+        [[1,1],[0, 2], [2, 2], [1,3]]
+      ]
+    end
+  end
+
+  describe '#beehive' do
+    it 'returns an array of all variations of beehive pattern' do
+      expect(subject.beehive(1,1)).to eq [
+        [[1, 1], [0, 2], [2, 2], [0, 3], [2, 3], [1, 4]],
+        [[1, 1], [2, 1], [0, 2], [3, 2], [1, 3], [2, 3]]
+      ]
+    end
+  end
+
+  describe '#blinker' do
+    it 'returns an array of all variations of blinker pattern' do
+      expect(subject.blinker(1,1)).to eq [
+        [[1, 1], [1, 2], [1, 3]],
+        [[1, 1], [2, 1], [3, 1]]
       ]
     end
   end
