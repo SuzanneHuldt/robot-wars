9806b57e866c186e5c5092b6096c39ef0420a791
Peter Johnstone <peter.wdj@gmail.com>
2018-01-14 17:55:16 +0000

    Refactor to make pattern an instance variable

--- lib/pattern.rb
+++ lib/pattern.rb
@@ -3,13 +3,19 @@ class Pattern
   def initialize(y, x)
     @y = y
     @x = x
+    @pattern = [
+      [[@x, @y], [@x + 1, @y], [@x, @y + 1]],
+      [[@x, @y], [@x + 1, @y], [@x + 1, @y + 1]],
+      [[@x, @y], [@x - 1, @y + 1], [@x, @y + 1]],
+      [[@x, @y], [@x, @y + 1], [@x + 1, @y + 1]]
+    ]
+
   end
 
   def build
     variations = []
-    variations << [[@x, @y], [@x + 1, @y], [@x, @y + 1]]
-    variations << [[@x, @y], [@x + 1, @y], [@x + 1, @y + 1]]
-    variations << [[@x, @y], [@x - 1, @y + 1], [@x, @y + 1]]
-    variations << [[@x, @y], [@x, @y + 1], [@x + 1, @y + 1]]
+    @pattern.each do |variation|
+      variations << variation
+    end
   end
 end
