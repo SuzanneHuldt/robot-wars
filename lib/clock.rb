987fbee48f603a71a3fa987d647d4747908d3e13
Eustaquio122 <tiagofsousa@gmail.com>
2018-01-14 12:27:59 +0000

    Adds Clock class

---
+++ lib/clock.rb
@@ -0,0 +1,23 @@
+class Clock
+  def initialize
+    @in = nil
+    @out = nil
+  end
+
+  def start
+    reset
+    @in = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+  end
+
+  def stop
+    @out = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+    (@out - @in) * 1000
+  end
+
+  private
+
+  def reset
+    @in = nil
+    @out = nil
+  end
+end
