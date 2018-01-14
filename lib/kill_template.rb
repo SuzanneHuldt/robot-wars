1621fbc79f0f0c221ca46946dfd3f8a9ed30a8c1
Peter Johnstone <peter.wdj@gmail.com>
2018-01-14 21:36:48 +0000

    Add methods to find each pattern on Riddles.io

--- lib/kill_template.rb
+++ lib/kill_template.rb
@@ -7,4 +7,24 @@ class KillTemplate
       [[x, y], [x, y + 1], [x + 1, y + 1]]
     ]
   end
+
+  def tub(x, y)
+    [
+      [[x, y], [x - 1, y + 1], [x + 1, y + 1], [x, y + 2]]
+    ]
+  end
+
+  def beehive(x, y)
+    [
+      [[x, y], [x - 1, y + 1], [x +1, y + 1], [x - 1, y + 2], [x + 1, y + 2], [x, y + 3]],
+      [[x, y], [x + 1, y], [x - 1, y + 1], [x +2, y + 1], [x, y + 2], [x + 1, y + 2]]
+    ]
+  end
+
+  def blinker(x, y)
+    [
+      [[x, y], [x, y + 1], [x, y + 2]],
+      [[x, y], [x + 1, y], [x + 2, y]]
+    ]
+  end
 end
