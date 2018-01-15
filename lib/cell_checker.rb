e894e23684b9d0dec986ac3e6665a98737635f7f
Robert Clayton <robbie.c.clayton@gmail.com>
2018-01-15 12:17:44 +0000

    split methods into remove and identify blank_grid_of_4

--- lib/cell_checker.rb
+++ lib/cell_checker.rb
@@ -1,9 +1,10 @@
 class CellChecker
-  attr_reader :valid_grid_coordinates
+  attr_reader :valid_grid_coordinates, :to_be_removed
 
   def initialize(field)
     @field = field
     @valid_grid_coordinates = []
+    @to_be_removed = []
   end
 
   def check_for_valid_grid_of_4
@@ -14,34 +15,29 @@ class CellChecker
     end
   end
 
-  def remove_blank_grid_of_4
-    @to_be_removed = []
+  def identify_blank_grid_of_4
+    to_be_removed = []
     @valid_grid_coordinates.each do |coordiates|
-
       coords = coordiates.split(",")
-      @x = coords[0]
-      @y = coords[1]
-      @x_loop = @x.to_i
-      @y_loop = @y.to_i
-
-      @remove_this_coordinate = false
-      @x_counter = 0
-
+      x_loop = coords[0].to_i
+      y_loop = coords[1].to_i
+      remove_this_coordinate = false
+      x_counter = 0
       4.times do
-        @y_counter = 0
+        y_counter = 0
         4.times do
-          @field[(@x_loop + @x_counter)] ||= []
-          @remove_this_coordinate = true if
-            @field[@x_loop + @x_counter][@y_loop + @y_counter] != '.'
-
-          @y_counter += 1
+          @field[(x_loop + x_counter)] ||= []
+          remove_this_coordinate = true if
+            @field[x_loop + x_counter][y_loop + y_counter] != '.'
+          y_counter += 1
         end
-        @x_counter += 1
+        x_counter += 1
       end
-
-    @to_be_removed << coordiates if @remove_this_coordinate == true
+    @to_be_removed << coordiates if remove_this_coordinate == false
     end
+  end
 
+  def remove_blank_grid_of_4
     @to_be_removed.each do |remove|
       @valid_grid_coordinates.delete(remove)
     end
