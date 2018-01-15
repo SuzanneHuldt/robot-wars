b63f36b2bd3a0877fe47d9de59bbd2dfb6a5c125
Peter Johnstone <peter.wdj@gmail.com>
2018-01-15 15:07:54 +0000

    Hard-code 4-length blank cell fragment

--- lib/fragment.rb
+++ lib/fragment.rb
@@ -1,5 +1,30 @@
 class Fragment
   def find(board)
+    all_board_coordinates = find_all_coordinates(board)
+    fragments = []
+    all_board_coordinates.each do |coordinates|
+      x = coordinates[0]
+      y = coordinates[1]
+      next if board[y][x] != '.'
+      next if board[y][x + 1] != '.'
+      next if board[y][x + 2] != '.'
+      next if board[y][x + 3] != '.'
+      next if board[y + 1][x] != '.'
+      next if board[y + 1][x + 3] != '.'
+      next if board[y + 2][x] != '.'
+      next if board[y + 2][x + 3] != '.'
+      next if board[y + 3][x] != '.'
+      next if board[y + 3][x + 1] != '.'
+      next if board[y + 3][x + 2] != '.'
+      next if board[y + 3][x + 3] != '.'
+      fragments << coordinates
+    end
+    fragments
+  end
+
+  private
+
+  def find_all_coordinates(board)
     row = 0
     all_board_coordinates = []
     while row < board.length do
@@ -13,5 +38,6 @@ class Fragment
       end
       row += 1
     end
+    all_board_coordinates
   end
 end
