6806f8ae063eeaeebc8e6f9e63ac08a93e721a0f
Robert Clayton <robbie.c.clayton@gmail.com>
2018-01-15 00:09:57 +0000

    test passing for check_for_valid_grid_of_4

--- lib/cell_checker.rb
+++ lib/cell_checker.rb
@@ -1,3 +1,23 @@
 class CellChecker
-  attr_reader :valid_grid_coordinates
+  attr_reader :valid_grid_coordinates, :field
+
+  def initialize(field)
+    @field = field
+    @valid_grid_coordinates = []
+  end
+
+  def check_for_valid_grid_of_4
+    15.times do |x|
+      13.times do |y|
+        @valid_grid_coordinates << "#{x},#{y}"
+      end
+    end
+
+    @valid_grid_coordinates.each do |coordiates|
+      coords = coordiates.split(",")
+      # p @field[coords[0].to_i][coords[1].to_i]
+    end
+  end
 end
+
+#&& (@field[y_loop][x_loop + 3] != nil)
