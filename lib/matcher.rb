6e5977b63e41d72514eb105a518294f1bdb9c5f3
Peter Johnstone <peter.wdj@gmail.com>
2018-01-15 16:02:07 +0000

    Prevent passing if pattern is found with additional cells included

--- lib/matcher.rb
+++ lib/matcher.rb
@@ -2,8 +2,8 @@ class Matcher
 
   def initialize(board)
     @board = board
-    @y = find_top_left_y_coordinate(@board)
-    @x = find_top_left_x_coordinate(@board[@y])
+    @y = find_top_left_y_coordinate
+    @x = find_top_left_x_coordinate
     @pattern = Pattern.new(@y, @x).build
   end
 
@@ -13,22 +13,32 @@ class Matcher
       variation.each do |coordinates|
         hits << [coordinates[1], coordinates[0]] if board[coordinates[1]][coordinates[0]] == '1'
       end
-      return hits if hits.length == variation.length
+      return hits if hits.length == variation.length && hits.length == total_alive_cells
     end
     'No matches'
   end
 
   private
 
-  def find_top_left_y_coordinate(board)
-    board.each do |row|
-      return board.index(row).to_i if row.include? '1'
+  def find_top_left_y_coordinate
+    @board.each do |row|
+      return @board.index(row).to_i if row.include? '1'
     end
   end
 
-  def find_top_left_x_coordinate(row)
-    row.each do |column|
-      return row.index(column).to_i if column.include? '1'
+  def find_top_left_x_coordinate
+    @board[@y].each do |column|
+      return @board[@y].index(column).to_i if column.include? '1'
     end
+  end
+
+  def total_alive_cells
+    counter = 0
+      @board.each_with_index do |row, y|
+        row.each_with_index do |cell, x|
+          counter += 1 if cell == '1' || cell == '0'
+        end
+      end
+    counter
   end
 end
