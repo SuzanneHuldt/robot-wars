7796fcebba2489f90de5d26f3cb2f826898b3fdc
Peter Johnstone <peter.wdj@gmail.com>
2018-01-15 15:07:11 +0000

    Correct test expectation to have coordinates in a 2D array

--- spec/fragment_spec.rb
+++ spec/fragment_spec.rb
@@ -11,7 +11,7 @@ describe Fragment do
         ['.', '.', '0', '1', '.', '.', '1', '1'],
         ['1', '.', '0', '.', '.', '.', '1', '0']
       ]
-      expect(subject.find(board)).to eq [1, 1]
+      expect(subject.find(board)).to eq [[1, 1]]
     end
   end
 end
