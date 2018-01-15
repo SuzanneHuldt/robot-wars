626df21f000aa8899c9155a0ef990767d5573d5f
Robert Clayton <robbie.c.clayton@gmail.com>
2018-01-15 17:24:08 +0000

    created five_by_five method
    creates a pattern for an empty outer five by five square

--- lib/fragment_library.rb
+++ lib/fragment_library.rb
@@ -29,4 +29,14 @@ class FragmentLibrary
       [x + 4, y + 4], [x + 5, y + 4]
     ]
   end
+
+  def five_by_five(x,y)
+    [
+      [x, y], [x + 1, y], [x + 2, y], [x + 3, y], [x + 4, y],
+      [x, y + 1], [x + 4, y + 1],
+      [x, y + 2], [x + 4, y + 2],
+      [x, y + 3], [x + 4, y + 3],
+      [x, y + 4], [x + 1, y + 4], [x + 2, y + 4], [x + 3, y + 4], [x + 4, y + 4]
+    ]
+  end
 end
--- spec/fragment_library_spec.rb
+++ spec/fragment_library_spec.rb
@@ -34,4 +34,16 @@ describe FragmentLibrary do
       ]
     end
   end
+
+  describe '#five_by_five' do
+    it 'should return the coordinates for a five by six square' do
+      expect(subject.five_by_five(1,1)).to eq [
+        [1,1], [2,1], [3,1], [4,1], [5,1],
+        [1,2],                      [5,2],
+        [1,3],                      [5,3],
+        [1,4],                      [5,4],
+        [1,5], [2,5], [3,5], [4,5], [5,5]
+      ]
+    end
+  end
 end
