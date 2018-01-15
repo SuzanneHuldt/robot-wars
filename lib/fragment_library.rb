fc25f92c4fd055e11ac647a5759c6d9bf249a719
Robert Clayton <robbie.c.clayton@gmail.com>
2018-01-15 17:17:08 +0000

    created five_by_six method
    creates a patter for an empty outer five by six square

--- lib/fragment_library.rb
+++ lib/fragment_library.rb
@@ -2,9 +2,20 @@ class FragmentLibrary
   def four_by_four(x, y)
     [
       [x, y], [x + 1, y], [x + 2, y], [x + 3, y],
-      [x, y + 1], [x + 3, y + 1], [x, y + 2],
-      [x + 3, y + 2], [x, y + 3], [x + 1, y + 3],
-      [x + 2, y + 3], [x + 3, y + 3]
+      [x, y + 1], [x + 3, y + 1],
+      [x, y + 2], [x + 3, y + 2],
+      [x, y + 3], [x + 1, y + 3], [x + 2, y + 3], [x + 3, y + 3]
+    ]
+  end
+
+  def five_by_six(x,y)
+    [
+      [x, y], [x + 1, y], [x + 2, y], [x + 3, y], [x + 4, y],
+      [x, y + 1], [x + 4, y + 1],
+      [x, y + 2], [x + 4, y + 2],
+      [x, y + 3], [x + 4, y + 3],
+      [x, y + 4], [x + 4, y + 4],
+      [x, y + 5], [x + 1, y + 5], [x + 2, y + 5], [x + 3, y + 5], [x + 4, y + 5]
     ]
   end
 end
--- spec/fragment_library_spec.rb
+++ spec/fragment_library_spec.rb
@@ -2,8 +2,22 @@ describe FragmentLibrary do
   describe '#four_by_four' do
     it 'should return the coordinates for a four by four square' do
       expect(subject.four_by_four(1,1)).to eq [
-        [1,1], [2,1], [3,1], [4,1], [1,2], [4,2],
-        [1,3], [4,3], [1,4], [2,4], [3,4], [4,4]
+        [1,1], [2,1], [3,1], [4,1],
+        [1,2],               [4,2],
+        [1,3],               [4,3],
+        [1,4], [2,4], [3,4], [4,4]
+      ]
+    end
+  end
+  describe '#five_by_six' do
+    it 'should return the coordinates for a five by six square' do
+      expect(subject.five_by_six(1,1)).to eq [
+        [1,1], [2,1], [3,1], [4,1], [5,1],
+        [1,2],                      [5,2],
+        [1,3],                      [5,3],
+        [1,4],                      [5,4],
+        [1,5],                      [5,5],
+        [1,6], [2,6], [3,6], [4,6], [5,6]
       ]
     end
   end
