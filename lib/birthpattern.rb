a5b3e9e09a62faa2006d2cc33db75b51e0898590
Suzanne Huldt <suzanne.huldt.16@ucl.ac.uk>
2018-01-16 15:22:41 +0000

    vertical search added

--- lib/birthpattern.rb
+++ lib/birthpattern.rb
@@ -1,12 +1,20 @@
 class BirthPattern
 
   def birth(field, array)
-    birth_cell(@hits) if find_birth(field, array)
+    moves = []
+    moves << birth_cell_horizontal(@hits) if find_birth_horizontal(field, array)
+    moves << birth_cell_vertical(@hits) if find_birth_vertical(field, array)
   end
 
-  def find_birth(field, array)
+  def find_birth_horizontal(field, array)
     for i in array
-      return match_square(field, get_x(i), get_y(i))
+      return match_square_horizontal(field, get_x(i), get_y(i))
+    end
+  end
+
+  def find_birth_vertical(field, array)
+    for i in array
+      return match_square_vertical(field, get_x(i), get_y(i))
     end
   end
 
@@ -18,24 +26,51 @@ class BirthPattern
     sub_array[1]
   end
 
-  def birth_cell(hits)
+  def birth_cell_horizontal(hits)
     var = hits.sample
     x = var[0].to_i
     y = var[2].to_i + 1
     cell = "#{x},#{y}"
   end
 
+  def birth_cell_vertical(hits)
+    var = hits.sample
+    x = var[0].to_i + 1
+    y = var[2].to_i
+    cell = "#{x},#{y}"
+  end
 
-  def match_square(field,x,y)
+
+  def match_square_horizontal(field,x,y)
     arr = []
     arr << (clear_horizontal(field,x,y) && clear_horizontal(field, x,(y+3)))
-    arr << (two_consecutive(field,x,(y+1)) ^ two_consecutive(field,x,(y+2)))
+    arr << (two_consecutive_across(field,x,(y+1)) ^ two_consecutive_across(field,x,(y+2)))
     arr.include? false ? false : true
   end
 
-  def clear_vertical(field,x,y)
+  def match_square_vertical(field,x,y)
+    arr = []
+    arr << (clear_vertical(field,x,y) && clear_vertical(field, (x+3),y))
+    arr << (two_consecutive_up(field,(x+1),y) ^ two_consecutive_up(field,(x+2),y))
+    p arr
+    if arr.include? false
+      false
+    else
+      true
+    end
+  #  arr.include? false ? false : true
   end
 
+  def clear_vertical(field, x, y)
+     hits = []
+     z = 1
+     while z < 5 do
+       y += 1
+       z += 1
+       hits << field[y][x] if field[y][x] != "."
+     end
+     hits.length == 0
+   end
 
   def clear_horizontal(field, x, y)
     hits = []
@@ -48,7 +83,7 @@ class BirthPattern
     hits.length == 0
   end
 
-  def two_consecutive(field, x, y)
+  def two_consecutive_across(field, x, y)
     @hits = []
     z = 1
     while z < 5 do
@@ -57,5 +92,16 @@ class BirthPattern
       @hits << "#{x},#{y}" if field[y][x] == "0"
     end
     @hits.length == 2 && @hits[1][0].to_i - @hits[0][0].to_i == 1
+  end
+
+  def two_consecutive_up(field, x, y)
+    @hits = []
+    z = 1
+    while z < 5 do
+      y += 1
+      z += 1
+      @hits << "#{x},#{y}" if field[y][x] == "0"
+    end
+    @hits.length == 2 && @hits[1][2].to_i - @hits[0][2].to_i == 1
   end
 end
