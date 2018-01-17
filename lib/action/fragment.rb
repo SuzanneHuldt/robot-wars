46680e444596edde173aa2fe2fba405d1a8dff33
Peter Johnstone <peter.wdj@gmail.com>
2018-01-17 12:40:34 +0000

    Simplify find_all_coordinates method to use each_with_index instead of each method with counters

--- lib/action/fragment.rb
+++ lib/action/fragment.rb
@@ -24,13 +24,11 @@ class Fragment
   def get_inner(board)
     fragments_coordinates = find(board)
     fragments = []
-
     fragments_coordinates.each_with_index do |fragment, index|
       x = fragment[0] + 1
       y = fragment[1] + 1
       width = @inner_dimensions[index][0]
       height = @inner_dimensions[index][1]
-
       width.times do |w|
         row = []
         height.times do |h|
@@ -38,27 +36,23 @@ class Fragment
         end
         fragments << row
       end
-
     end
     fragments
   end
 
   private
 
-  def find_all_coordinates(board)
-    row = 0
+  def find_all_coordinates(fragment)
     all_board_coordinates = []
-    while row < board.length do
-      column = 0
-      while column < board[row].length do
+    fragment.each_with_index do |row, y|
+      row.each_with_index do |cell, x|
         coordinates = []
-        coordinates << row
-        coordinates << column
+        coordinates << x
+        coordinates << y
         all_board_coordinates << coordinates
-        column += 1
       end
-      row += 1
     end
     all_board_coordinates
   end
+
 end
