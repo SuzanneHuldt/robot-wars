8d9196bc9204079f12b4f1f7e0d1102dfc31ffcd
Peter Johnstone <peter.wdj@gmail.com>
2018-01-17 16:55:53 +0000

    Extract logic for #get_inner to private methods

--- lib/action/fragment.rb
+++ lib/action/fragment.rb
@@ -5,8 +5,7 @@ class Fragment
     fragments = []
     @inner_dimensions = []
     all_board_coordinates.each do |coordinates|
-      x = coordinates[0]
-      y = coordinates[1]
+      x, y = assign_board_x_and_y(coordinates)
       fragment_templates = FragmentLibrary.new.all_fragments(x, y)
       owned_cell = false
       fragment_templates.each do |template|
@@ -23,21 +22,7 @@ class Fragment
 
   def get_inner(board)
     fragments_coordinates = find(board)
-    fragments = []
-    fragments_coordinates.each_with_index do |fragment, index|
-      x = fragment[0] + 1
-      y = fragment[1] + 1
-      width = @inner_dimensions[index][0]
-      height = @inner_dimensions[index][1]
-      width.times do |w|
-        row = []
-        height.times do |h|
-          row << board[x + w][y + h]
-        end
-        fragments << row
-      end
-    end
-    fragments
+    build_fragment_array(board, fragments_coordinates)
   end
 
   private
@@ -46,13 +31,56 @@ class Fragment
     all_board_coordinates = []
     fragment.each_with_index do |row, y|
       row.each_with_index do |cell, x|
-        coordinates = []
-        coordinates << x
-        coordinates << y
-        all_board_coordinates << coordinates
+        all_board_coordinates << create_coordinates(x, y)
       end
     end
     all_board_coordinates
+  end
+
+  def create_coordinates(x, y)
+    coordinates = []
+    coordinates << x
+    coordinates << y
+  end
+
+  def build_fragment(board, x, y, width, height)
+    fragment = []
+    width.times do |w|
+      row = []
+      height.times do |h|
+        row << board[x + w][y + h]
+      end
+      fragment << row
+    end
+    fragment
+  end
+
+  def build_fragment_array(board, coordinates)
+    fragments = []
+    coordinates.each_with_index do |fragment, index|
+      x, y = assign_fragment_x_and_y(fragment)
+      width, height = assign_width_and_height(index)
+      fragments << build_fragment(board, x, y, width, height)
+    end
+    fragments
+  end
+
+  def assign_board_x_and_y(coordinates)
+    x = coordinates[0]
+    y = coordinates[1]
+    return x, y
+  end
+
+  def assign_fragment_x_and_y(fragment)
+    x = fragment[0] + 1
+    y = fragment[1] + 1
+    return x, y
+  end
+
+  def assign_width_and_height(index)
+    width = @inner_dimensions[index][0]
+    height = @inner_dimensions[index][1]
+    return width, height
   end
 
 end
